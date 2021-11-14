60c5664e04d06112b96fe821e12d107028e10f19
TheScrewdriver <kinoide56@gmail.com>
2021-11-14 18:39:21 +0100

    Colorize active edges

--- Path_Graphs.py
+++ Path_Graphs.py
@@ -18,17 +18,20 @@ global edges
 
 fig = plt.figure()
 G = nx.Graph()
-nodes_names = "ABCD"
-G.add_nodes_from(list(nodes_names))	
+nodes_names = ["Start","A","B","C","Target"]
+G.add_nodes_from(nodes_names)
+nodes_number = len(nodes_names)
+maximum_edges = int((nodes_number * (nodes_number - 1))/2)
 
 ##Create random edges##
 
-for k in range(2*len(nodes_names)):
+for k in range(maximum_edges):
 
 	alphabet_temp = nodes_names
 	first_random = random.choice(alphabet_temp)
-	alphabet_temp = alphabet_temp.replace(first_random,"")
+	alphabet_temp.remove(first_random)
 	second_random = random.choice(alphabet_temp)
+	alphabet_temp.append(first_random)
 	if ([first_random, second_random] not in list(G.edges)):
 		G.add_edge(first_random, second_random, weight = 0)
 
@@ -36,33 +39,63 @@ for k in range(2*len(nodes_names)):
 
 pos = nx.spring_layout(G)
 nx.draw_networkx(G,pos)
+nx.draw_networkx_nodes(G, pos, node_size = 5000, node_color = "tab:green")
 
 ##Create lists of edges and their weight##
 
 edges = list(G.edges)
 weight_edge = np.zeros(len(edges), dtype = int)
 
+##Colorize active edges##
+
+def color_edges(active):
+
+	nx.draw_networkx_edges(
+    G,
+    pos,
+    edgelist = edges,
+    width=8,
+    alpha=1,
+    edge_color="tab:blue",
+	)
+	
+	nx.draw_networkx_edges(
+    G,
+    pos,
+    edgelist=active,
+    width=8,
+    alpha=1,
+    edge_color="tab:red",
+	)
+	
 def update(frame):
 
 	global weight_edge
 	global edges
 
+	chosen = random.randint(0, len(weight_edge) - 1)
+	
 	##Clear edges labels##
 	
 	G.remove_edges_from(edges)
-	
+	active = []
+
 	##Recreate edges with their new weight##
 
 	for k in range(len(edges)):
+		
 		G.add_edge(edges[k][0], edges[k][1], weight = weight_edge[k])
-	labels = nx.get_edge_attributes(G,'weight')
-	nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+		if (k == chosen):
+			active.append([edges[k][0],edges[k][1]])
 
+	color_edges(active)	
+	labels = nx.get_edge_attributes(G, 'weight')
+	nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+	
 	##Increase or Decrease randomly an edge weight##
 
-	chosen = random.randint(0, len(weight_edge) - 1)
 	weight_edge[chosen] = random.choice([weight_edge[chosen] - 1, weight_edge[chosen] + 1])
-
+	
 	#time.sleep(1)
 
 ##Trigger the animation##
