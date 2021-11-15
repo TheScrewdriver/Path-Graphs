d32c5f1fa2b547df5db54d71e958bfd0017a71b5
TheScrewdriver <kinoide56@gmail.com>
2021-11-16 00:05:02 +0100

    Better display of road capacity, dynamic-node-label in progress (commented)

--- Path_Graphs.py
+++ Path_Graphs.py
@@ -2,50 +2,27 @@ from matplotlib import pyplot as plt, animation
 import numpy as np
 import networkx as nx
 import random
-import time 
+import time
+import string
 
-##Display Configuration##
+##########FUNCTIONS##########
 
-plt.rcParams["figure.figsize"] = [16, 7.50]
-plt.rcParams["figure.autolayout"] = True
+##Build Node list##
 
-##Global Variables Initialization##
-
-global weight_edge
-global edges
-
-##Create Graph with its nodes##
-
-fig = plt.figure()
-G = nx.Graph()
-nodes_names = ["Start","A","B","C","Target"]
-G.add_nodes_from(nodes_names)
-nodes_number = len(nodes_names)
-maximum_edges = int((nodes_number * (nodes_number - 1))/2)
-
-##Create random edges##
-
-for k in range(maximum_edges):
-
-	alphabet_temp = nodes_names
-	first_random = random.choice(alphabet_temp)
-	alphabet_temp.remove(first_random)
-	second_random = random.choice(alphabet_temp)
-	alphabet_temp.append(first_random)
+def node_list(node_number):
 	
-	if ([first_random, second_random] not in list(G.edges)):
-		G.add_edge(first_random, second_random, weight = 0)
+	L = []
 
-##Display Graph without edges labels##
+	alphabet = list(string.ascii_uppercase)
+	
+	if (node_number <= 1 or node_number - 2 > len(alphabet)):
+		return L
 
-pos = nx.spring_layout(G)
-nx.draw_networkx(G,pos)
-nx.draw_networkx_nodes(G, pos, node_size = 5000, node_color = "tab:green")
-
-##Create lists of edges and their weight##
-
-edges = list(G.edges)
-weight_edge = np.zeros([len(edges), 2], dtype = int)
+	L.append("Start")
+	for k in range(node_number - 2):
+		L.append(alphabet[k])
+	L.append("Target")
+	return L
 
 ##Colorize active edges##
 
@@ -69,14 +46,81 @@ def color_edges(active):
     edge_color="tab:red",
 	)
 
+##Create a list of random edge capacity##
+
+def capacity_generator(edge_list, max_capacity):
+	
+	L = []
+	for k in range(len(edge_list)):
+		n = random.randint(1, max_capacity)
+		L.append(n)
+	return L
+
+#def car_number_on_nodes(node, score):
+#
+#	mapping = 
+#	G = nx.relabel_nodes(G, mapping)
+	
+###END(FUNCTIONS)###
+
+##Display Configuration##
+
+plt.rcParams["figure.figsize"] = [16, 7.50]
+plt.rcParams["figure.autolayout"] = True
+
+##Global Variables Initialization##
+
+global weight_edge
+global edges
+global pos
+global nodes_names
+global nodes_number
+
+nodes_number = 5 #max = 28
+
+##Create Graph with its nodes##
+
+fig = plt.figure()
+G = nx.Graph()
+nodes_names = node_list(nodes_number)
+G.add_nodes_from(nodes_names)
+maximum_edges = int((nodes_number * (nodes_number - 1))/2)
+
+##Create random edges##
+
+for k in range(maximum_edges):
+
+	nodes_temp = nodes_names
+	random_edge = random.choices(nodes_temp, k=2)
+	
+	if ([random_edge[0], random_edge[1]] not in list(G.edges)):
+		if ([random_edge[1], random_edge[0]] not in list(G.edges)):
+			if (random_edge[0] != random_edge[1]):
+				G.add_edge(random_edge[0], random_edge[1], weight = 0)
+
+##Display Graph without edges labels##
+
+pos = nx.spring_layout(G)
+nx.draw_networkx(G,pos)
+nx.draw_networkx_nodes(G, pos, node_size = 5000, node_color = "tab:green")
+
+##Create lists of edges and their weight##
+
+edges = list(G.edges)
+weight_edge = np.zeros(len(edges), dtype = int)
+capacity_edge = capacity_generator(edges, 5)
+
 ##Update animation##
 	
 def update(frame):
 
 	global weight_edge
 	global edges
+	global nodes_names
+	global pos
+	global nodes_number
 
-	chosen = random.randint(0, len(weight_edge) - 1)
+	chosen_edge = random.randint(0, len(weight_edge))
 	
 	##Clear edges labels##
 	
@@ -87,17 +131,33 @@ def update(frame):
 
 	for k in range(len(edges)):
 		
-		G.add_edge(edges[k][0], edges[k][1], weight = weight_edge[k])
-		if (k == chosen):
-			active.append([edges[k][0],edges[k][1]])
+		G.add_edge(edges[k][0], edges[k][1], weight = str(weight_edge[k]) + "/" + str(capacity_edge[k]))
+		#if (k == chosen_edge):
+		#	active.append([edges[k][0],edges[k][1]])
 
-	color_edges(active)	
+	#color_edges(active)
+	
 	elabels = nx.get_edge_attributes(G, 'weight')
 	nx.draw_networkx_edge_labels(G, pos, edge_labels = elabels)
 	
+	##Refresh node labels##
+
+#	node_score = np.zeros(nodes_number, dtype = int)	
+#	nodes_names_new = np.zeros(nodes_number, dtype = str)
+#	
+#	for k in range(len(nodes_names)):
+#		
+#		nodes_names_new[k] = nodes_names[k] + "\n"
+#		pos[nodes_names_new[k]] = pos.pop(nodes_names[k])
+#	
+#	print("post-pos :", pos)
+#	G.add_nodes_from(nodes_names_new)
+#	nx.draw_networkx(G, pos)
+#	nx.draw_networkx_nodes(G, pos, node_size = 5000)
+	
 	##Increase or Decrease randomly an edge weight##
 
-	weight_edge[chosen][0] = random.choice([weight_edge[chosen][0] - 1, weight_edge[chosen][0] + 1])
+	#weight_edge[chosen_edge][0] = random.choice([weight_edge[chosen_edge][0] - 1, weight_edge[chosen_edge][0] + 1])
 	
 	#time.sleep(1)
 
